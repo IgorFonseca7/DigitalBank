7a1cb87bcda0882b580d30e5387db6438b0974b3
IgorFonseca7 <igor.galaxy2015@gmail.com>
2024-05-26 16:30:55 -0300

    Adicionado dados do cliente

--- src/Desafio.java
+++ src/Desafio.java
@@ -1,14 +1,31 @@
+import java.util.Scanner;
+
 public class Desafio {
     public static void main(String[] args) {
         String nome = "Tony Stark";
         String tipoConta = "Corrente";
         double saldo = 1599.99;
+        int opcao = 0;
 
-        System.out.println("********************");
+        System.out.println("***********************");
         System.out.println("\nNome do Cliente:" + nome);
         System.out.println("Tipo conta: " + tipoConta);
         System.out.println("Saldo atual: " + saldo);
         System.out.println("\n*********************");
+
+        Scanner leitura = new Scanner(System.in);
+        String menu = """
+                ** Digite sua opcao **
+                1 - Consultar Saldo
+                2 - Transferir Valor
+                3 - Receber Valor
+                4 - Sair
+                """;
+
+        while (opcao!= 4){
+            System.out.println(menu);
+            opcao = leitura.nextInt();
+        }
     }
 }
 
